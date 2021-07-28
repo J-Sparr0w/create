9c0787651750645d07a165fe1dc3ab394582613a
J-Sparr0w <18arjun@gmail.com>
2021-07-28 11:31:54 +0530

    completed the project and it is working perfectly

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const prompt = require('prompt-sync')({ sigint: true });
 const fs = require('fs');
 const html = require('./boiler');
-
 let [type, project] = require('process').argv.slice(2);
 
 
-if (!project) {
-    project = prompt('project name:');
-}
-const projectPath=`${process.cwd()}/${project}`
-const srcPath = `${project}/src`;
-fs.mkdirSync(projectPath);
-fs.mkdirSync(srcPath);
+function writeBoiler() {
+     
 
-if (type === 'v' || type === 'vanilla' || !type) {
-    fs.writeFileSync(`${srcPath}/index.html`, html.getHTML(project));
-    console.log("html added");
-    fs.writeFileSync(`${srcPath}/styles.css`, html.cssBoiler);
-    console.log("css added");
-    fs.writeFileSync(`${srcPath}/index.js`,html.jsBoiler);
-    console.log("index.js added");
-}
+    if (!project) {
+        project = prompt('project name:');
+    }
+    const projectPath = `${process.cwd()}/${project}`
+    const srcPath = `${project}/src`;
+    fs.mkdirSync(projectPath);
+    fs.mkdirSync(srcPath);
 
-else {
-    console.log("type has not been entered or it is invalid");
+    if (type === 'v' || type === 'vanilla') {
+        fs.writeFileSync(`${srcPath}/index.html`, html.getHTML(project));
+        console.log("html added");
+        fs.writeFileSync(`${srcPath}/styles.css`, html.cssBoiler);
+        console.log("css added");
+        fs.writeFileSync(`${srcPath}/index.js`, html.jsBoiler);
+        console.log("index.js added");
+    }
+
 }
+exports.writeBoiler = writeBoiler;
